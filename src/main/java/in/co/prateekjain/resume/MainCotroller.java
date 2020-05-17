1261204803c22010f80f85d0cb01cae74f3a02dd
prateeekjain <prateekjainbit@gmail.com>
2020-05-18 02:43:10 +0530

    sendgrid test

--- src/main/java/in/co/prateekjain/resume/MainCotroller.java
+++ src/main/java/in/co/prateekjain/resume/MainCotroller.java
@@ -1,6 +1,7 @@
 package in.co.prateekjain.resume;
 
 import java.io.IOException;
+import com.sendgrid.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,10 +27,9 @@ import com.sendgrid.helpers.mail.objects.Email;
 
 @Controller
 public class MainCotroller {
-	@Autowired
-    private JavaMailSender javaMailSender;
-	
-	
+//	@Autowired
+//    private JavaMailSender javaMailSender;
+
 	
 	@RequestMapping("/")
 	public String home()
@@ -40,21 +40,24 @@ public class MainCotroller {
 	@RequestMapping(value= "/sendMail", method = RequestMethod.GET)
 	public ModelAndView sendMail(@RequestParam(value = "contactName")String contactName,@RequestParam(value = "contactEmail") String contactEmail, @RequestParam(value = "contactSubject")String contactSubject, @RequestParam(value = "contactMessage")String contactMessage) throws IOException
 		{
-			try {
+//			try {
+//		
+//	        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+//	        Request request = new Request();
+//	        request.setMethod(Method.POST);
+//	        request.setEndpoint("mail/send");
+//	        request.setBody(contactName+"\n"+contactEmail+"\n"+contactSubject+"\n"+contactMessage);
+//	        Response response = sg.api(request);
+//	        System.out.println(response.getStatusCode());
+//	        System.out.println(response.getBody());
+//	        System.out.println(response.getHeaders());
+//	      } catch (IOException ex) {
+//	        throw ex;
+//	      }
+		 
 		
-	        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-	        Request request = new Request();
-	        request.setMethod(Method.POST);
-	        request.setEndpoint("mail/send");
-	        request.setBody(contactName+"\n"+contactEmail+"\n"+contactSubject+"\n"+contactMessage);
-	        Response response = sg.api(request);
-	        System.out.println(response.getStatusCode());
-	        System.out.println(response.getBody());
-	        System.out.println(response.getHeaders());
-	      } catch (IOException ex) {
-	        throw ex;
-	      }
-//		 SimpleMailMessage msg = new SimpleMailMessage();
+		
+//		SimpleMailMessage msg = new SimpleMailMessage();
 //	        msg.setTo("prateek_jain@programmer.net");
 //
 //	        msg.setSubject("Resume Contact Email from your website :"+contactSubject);
@@ -64,6 +67,26 @@ public class MainCotroller {
 //		
 //	
 //		System.out.println("function called");
+
+		Email from = new Email("prateekjainbit@gmail.com");
+	    String subject = contactSubject;
+	    Email to = new Email("prateekjainbitm@gmail.com");
+	    Content content = new Content("text/plain", contactEmail+"\n"+contactMessage);
+	    Mail mail = new Mail(from, subject, to, content);
+
+	    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+	    Request request = new Request();
+	    try {
+	      request.setMethod(Method.POST);
+	      request.setEndpoint("mail/send");
+	      request.setBody(mail.build());
+	      Response response = sg.api(request);
+	      System.out.println(response.getStatusCode());
+	      System.out.println(response.getBody());
+	      System.out.println(response.getHeaders());
+	    } catch (IOException ex) {
+	      throw ex;
+	    }
 		return new ModelAndView("index.html");
 	}
 	
